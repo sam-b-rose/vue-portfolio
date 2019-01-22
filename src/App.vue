10377f8b468e7010e944b12efbc23673384e9187
samrose3 <samrose3@gmail.com>
2019-01-22 17:39:06 -0600

    Remove old Do component

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 <script>
 import Hero from './components/Hero';
 import About from './components/About';
-import Do from './components/Do';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
@@ -27,7 +26,6 @@ export default {
   components: {
     Hero,
     About,
-    Do,
     Projects,
     Skills,
     Fork,
